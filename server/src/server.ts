ee05be734db07e2305ccb8d2941e9f62682f3623
Kevyn Gonçalves <kevynredlotus@gmail.com>
2023-01-17 17:46:08 -0300

    initial commit

---
+++ server/src/server.ts
@@ -0,0 +1,18 @@
+import Fastify from 'fastify';
+import cors from '@fastify/cors';
+import { PrismaClient } from '@prisma/client';
+
+const app = Fastify();
+app.register(cors);
+const prisma = new PrismaClient();
+
+app.get('/', async () => {
+    const habits = await prisma.habit.findMany();
+    return { habits }
+})
+
+app.listen({
+    port: 3333,
+}).then(() => {
+    console.log("Server running at port:", 3333)
+})
